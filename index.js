51c9c00fe95df66207ebc9c1fd2810567e437506
Chris Stevenson <chrislstevenson@gmail.com>
2024-01-26 16:18:41 -0500

    Created classes, did quick test

--- index.js
+++ index.js
@@ -4,3 +4,33 @@
  * - based on input, a 300 x 200 pixel SVG file is created named "logo.svg"
  * - when finished, display message "Generated logo.svg" on CL
  */
+
+// TODO import inquirer, shapes, fs (promise version)
+const shapes = require('./lib/shapes')
+
+
+// get input from user
+// eventually want to give them drop-down auto-complete functionality if at all possible
+
+// strictly for testing purposes
+const logoInput = {
+  logoChars: "LOL",
+  textColor: "white",
+  shapeColor: "darkblue",
+  shape: "circle" // options are circle, triangle, square
+};
+
+
+// then use the input to build the shape (need to import the shapes module)
+// shape is rendered ihe shapes.js functionality and returned here
+// then it is output to a file
+
+const logo = new shapes.Triangle("CLS", "cyan", "darkblue");
+const output = `
+${logo.svgOpen()}
+${logo.render()}
+${logo.svgText()}
+${logo.svgClose()}
+`
+
+console.log(output);
