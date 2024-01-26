4c5e3220c73ec5a009b6c67973ca00a5601f879f
Chris Stevenson <chrislstevenson@gmail.com>
2024-01-26 10:46:46 -0500

    Setup with TODOs

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+ * Functionality:
+ * - prompts user for input: text (up to 3 chars), text color, shape, chape color
+ * - based on input, a 300 x 200 pixel SVG file is created named "logo.svg"
+ * - when finished, display message "Generated logo.svg" on CL
+ */
---
+++ test/shapes.test.js
@@ -0,0 +1,10 @@
+// TODO import Shapes class
+// TODO test each class for render() method that returns a string for the corresponding SVG file with the given shape color; see below
+/**
+ * Example of a test that should pass:
+
+const shape = new Triangle();
+shape.setColor("blue");
+expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+
+**/
