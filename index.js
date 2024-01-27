e521c155f6c8675e253fede1c203e4773da75059
Chris Stevenson <chrislstevenson@gmail.com>
2024-01-26 19:50:53 -0500

    Minimum viable product
    - no testing yet

--- index.js
+++ index.js
@@ -1,36 +1,75 @@
-/*
- * Functionality:
- * - prompts user for input: text (up to 3 chars), text color, shape, chape color
- * - based on input, a 300 x 200 pixel SVG file is created named "logo.svg"
- * - when finished, display message "Generated logo.svg" on CL
- */
+///////////////////////////////////////////////////////////////////////////////
+//                             SVG Logo Generator                            //
+///////////////////////////////////////////////////////////////////////////////
 
-// TODO import inquirer, shapes, fs (promise version)
-const shapes = require('./lib/shapes')
+/**
+ * This program gathers input from the user (terminal interface) and generates
+ * the corresponding SVG logo
+ **/
 
+// import shapes, inquirer, fs (promise version)
+const { Circle, Square, Triangle } = require('./lib/shapes')
+const inquirer = require('inquirer');
+const { writeFile } = require('fs/promises');
 
-// get input from user
-// eventually want to give them drop-down auto-complete functionality if at all possible
+// main function run when node index is input at CL
+function run() {
+  const prompt = {
+    logoChars: "What text (max 3 chars) do you want in your logo?",
+    textColor: "What color do you want the text?",
+    shape: "What shape do you want for your logo?",
+    shapeColor: "What fill color do you want for the shape?"
+  };
+  inquirer.prompt([
+    { // text in logo
+      type: 'input', name: 'logoChars', message: prompt.logoChars,
+      validate: value => {
+        if (value.length > 3) return "No more than three characters allowed";
+        else return true;
+      }
+    },
+    { // color of the text; eventually (hopefully) an autocomplete drop-down
+      type: 'input', name: 'textColor', message: prompt.textColor
+    },
+    { // logo shape: circle, square, triangle
+      type: 'list', name: 'shape', message: prompt.shape,
+      choices: ['circle', 'square', 'triangle']
+    },
+    { // logo fill color; eventually (hopefully) an autocomplete drop-down
+      type: 'input', name: 'shapeColor', message: prompt.shapeColor,
+    }
+  ]).then(generateSVG)
+    .then(output => {
+      writeFile('logo.svg', output);
+      console.log('Generated logo.svg')
+    })
+    .catch(err => console.log(err));
+}
 
-// strictly for testing purposes
-const logoInput = {
-  logoChars: "LOL",
-  textColor: "white",
-  shapeColor: "darkblue",
-  shape: "circle" // options are circle, triangle, square
-};
-
-
-// then use the input to build the shape (need to import the shapes module)
-// shape is rendered ihe shapes.js functionality and returned here
-// then it is output to a file
-
-const logo = new shapes.Triangle("CLS", "cyan", "darkblue");
-const output = `
+// function to generate SVG text from the user input
+// input is the data object (resolved promise) gathered by inquirer
+function generateSVG({ logoChars, textColor, shape, shapeColor }) {
+  let logo;
+  switch (shape) {
+    // object depends on user choice of shape
+    case 'circle':
+      logo = new Circle(logoChars, textColor, shapeColor);
+      break;
+    case 'square':
+      logo = new Square(logoChars, textColor, shapeColor);
+      break;
+    case 'triangle':
+      logo = new Triangle(logoChars, textColor, shapeColor);
+      break;
+  }
+  const output = `
 ${logo.svgOpen()}
-${logo.render()}
-${logo.svgText()}
+  ${logo.render()}
+  ${logo.svgText()}
 ${logo.svgClose()}
 `
+  return output;
+}
 
-console.log(output);
+// run the main program
+run();
